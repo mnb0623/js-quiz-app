5c1aadbc0f86bbef014ebc4ec4a27d01d71310de
MNBfromCatalina <boomboom0623@gmail.com>
2020-10-17 19:18:42 +0900

    クイズ機能作成

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,57 @@
-import 'bootstrap';
+const quiz = [
+  {
+    question: "your first name is ?",
+    answers: ["mnb", "dsfamnb", "mfsanb", "mnbdsa"],
+    correct: "mnb",
+  },
+  {
+    question: "your last name is ?",
+    answers: ["mnb", "fukui", "mfsanb", "mnbdsa"],
+    correct: "fukui",
+  },
+  {
+    question: "your borother is ?",
+    answers: ["mnb", "dsfamnb", "hjm", "mnbdsa"],
+    correct: "hjm",
+  },
+];
 
-// Popover の処理を追加してみた
-$(function () {
-  $("button[data-toggle='popover']").popover({
-    container: 'body'
-  })
-})
+const quizLength = quiz.length;
+let quizIndex = 0;
+const $button = document.getElementsByTagName("button");
+const buttonLength = $button.length;
 
-console.log('ddd;lddss')
+// 問題文、選択肢を定義
+const setupQuiz = function () {
+  document.getElementById("js-question").textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
+  while (buttonIndex < buttonLength) {
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+    buttonIndex++;
+  }
+};
+setupQuiz();
+
+const clickHandler = (e) => {
+  if (quiz[quizIndex].correct === e.target.textContent) {
+    window.alert("OK");
+  } else {
+    window.alert("NG");
+  }
+  quizIndex++;
+
+  if (quizIndex < quizLength) {
+    // 問題数がまだあれば実行
+    setupQuiz()
+  } else {
+    window.alert("end");
+  }
+};
+
+let handleIndex = 0;
+while (handleIndex < buttonLength) {
+  $button[handleIndex].addEventListener("click", (e) => {
+    clickHandler(e);
+  });
+  handleIndex++;
+}
